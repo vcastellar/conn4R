4536ffbeb43202976b7e407b64a1f545c2774e19
vcastellar <vic.sebastia@gmail.com>
2024-11-23 19:05:33 +0100

    mejoras, todavía no funciona correctamente

--- R/generarPosicion.R
+++ R/generarPosicion.R
@@ -5,33 +5,43 @@
 #' @param tablero matriz 6 x 7 que representa la situación del tablero de juego.
 #' @param turno
 #' @examples
-#' tablero <- crear_posicion_aleatoria()
+#' tablero <- .crear_posicion_aleatoria(42, 1)
 #' visualizar_tablero(tablero)
-#'
-crear_posicion_aleatoria <- function(profundidad = 10, turno = 1) {
-  i <- 0
-  turnoSiguiente <- turno
-  tablero <- reiniciar_tablero()
-  for (i in 1:profundidad) {
+.crear_posicion_aleatoria <- function(profundidad = 10, turno = 1) {
 
-    # validar que el juego ha terminado. Si es así, se reinicia el tablero
-    if (juego_terminado(tablero) & i < profundidad) {
-      tablero <- reiniciar_tablero()
-      i <- 0
-      turnoSiguiente <- turno
+
+  repeat {
+
+    turnoSiguiente <- turno
+    tablero <- reiniciar_tablero()
+    reiniciar <- FALSE
+
+    for (i in 1:profundidad) {
+
+      # validar que el juego ha terminado. Si es así, se reinicia el tablero
+      if (juego_terminado(tablero)) {
+        reiniciar <- TRUE
+        evaluar_posicion(tablero, 1)
+        break
+      }
+
+      # se elige una jugada al azar
+      jugadas_posibles <- jugadas_disponibles(tablero)
+
+      jugada <- sample(jugadas_posibles, 1)
+      tablero <- realizar_jugada(tablero, jugada, turnoSiguiente)
+
+      turnoSiguiente <- (turnoSiguiente %% 2) + 1
+
+    }
+
+    if (!reiniciar) {
       break
     }
 
-    # se elige una jugada al azar
-    jugadas_posibles <- jugadas_disponibles(tablero)
-
-    jugada <- sample(jugadas_posibles, 1)
-    tablero <- realizar_jugada(tablero, jugada, turnoSiguiente)
-
-    turnoSiguiente <- (turnoSiguiente %% 2) + 1
-
   }
 
+  print(i)
   return(tablero)
 
 }
