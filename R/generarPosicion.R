535704a22d4c72996116ec2721b04391bf39442d
vcastellar <vic.sebastia@gmail.com>
2024-11-24 09:51:49 +0100

    versión funcional de generar tablero aleatorio

--- R/generarPosicion.R
+++ R/generarPosicion.R
@@ -1,47 +1,53 @@
 #' Crear posición aleatoria
 #'
-#' @description dada una situacvión en el tablero de juego, devuelve las posibles
-#'   jugadas existentes: columnas no completadas
+#' @description crea un tablero aleatorio de cierta profundidad
 #' @param tablero matriz 6 x 7 que representa la situación del tablero de juego.
 #' @param turno
+#' @return Una lista con los siguientes elementos
+#' \itemize{
+#'   \item tablero - matriz 6 x 7 que representa el tablero generado
+#'   \item turnoUltimo - turno del jugador que ha realizado la última jugada en el tablero generado
+#' }
 #' @examples
-#' tablero <- .crear_posicion_aleatoria(42, 1)
-#' visualizar_tablero(tablero)
-.crear_posicion_aleatoria <- function(profundidad = 10, turno = 1) {
+#' tablero <- crear_posicion_aleatoria(20, 1)
+#' visualizar_tablero(tablero$tablero)
+#' tablero$turnoUltimo
+#' sum(tablero == 1)
+#' sum(tablero == 2)
+crear_posicion_aleatoria <- function(profundidad = 10, turno = 1) {
 
+  reiniciar <- TRUE
 
-  repeat {
-
-    turnoSiguiente <- turno
-    tablero <- reiniciar_tablero()
+  while (reiniciar) {
     reiniciar <- FALSE
 
-    for (i in 1:profundidad) {
+    turnoAux <- turno
+    tablero <- reiniciar_tablero()
 
-      # validar que el juego ha terminado. Si es así, se reinicia el tablero
-      if (juego_terminado(tablero)) {
-        reiniciar <- TRUE
-        evaluar_posicion(tablero, 1)
-        break
-      }
+
+    for (i in 1:profundidad) {
 
       # se elige una jugada al azar
       jugadas_posibles <- jugadas_disponibles(tablero)
 
-      jugada <- sample(jugadas_posibles, 1)
-      tablero <- realizar_jugada(tablero, jugada, turnoSiguiente)
+      jugada <- unlist(sample(as.list(jugadas_posibles), size = 1))
+      tablero <- realizar_jugada(tablero, jugada, turnoAux)
 
-      turnoSiguiente <- (turnoSiguiente %% 2) + 1
+      # validar que el juego ha terminado. Si es así, se reinicia el tablero
+      if (juego_terminado(tablero) & i < 42) {
+        reiniciar <- TRUE
+        break
 
-    }
+      }
 
-    if (!reiniciar) {
-      break
+      turnoAux <- (turnoAux %% 2) + 1
+
     }
 
   }
-
-  print(i)
-  return(tablero)
+  print(sum(tablero == 1))
+  print(sum(tablero == 2))
+  print(turnoAux)
+  return(list(tablero = tablero, turnoUltimo = turnoAux))
 
 }
