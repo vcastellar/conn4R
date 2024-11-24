2630f06f8a2f10f50a401a35469e2a76f48d290b
vcastellar <vic.sebastia@gmail.com>
2024-11-24 10:42:35 +0100

    corrección de error que involucra a "linea"

--- R/iniciar_partida.R
+++ R/iniciar_partida.R
@@ -65,7 +65,6 @@ iniciar_partida <- function(profundidad = 5, turno = 1) {
 
     print(paste0("valoracion IA: ", evaluar_posicion(tablero, 2)))
     print(paste0("jugada realizada: ", mejor_jugada_IA$jugada))
-    print(mejor_jugada_IA$linea)
 
 
     if (juego_terminado(tablero)) {
