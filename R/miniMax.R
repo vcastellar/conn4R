22de249a3d8a19388b09c9ffc8cb42efd4c4b93f
vcastellar <vic.sebastia@gmail.com>
2024-12-02 18:31:28 +0100

    documentación y formateo del codigo

--- R/miniMax.R
+++ R/miniMax.R
@@ -1,27 +1,50 @@
-# función que mediante un algoritmo mini-max la IA decide cuál es su mejor
-# jugada dada una cierta posición del tablero para conseguir la victoria en el
-#  desarrollo de la partida hasta su finalización.
-# La evaluacion de la posición la debe hacer la función evaluate_position anterior
-# prueba
+#' algoritmo minimax
+#'
+#' @description función que mediante un algoritmo mini-max la IA decide cuál es su mejor
+#' jugada dada una cierta posición del tablero 
 
-mejor_jugada_minmax <- function(tablero, profundidad, maximizando_jugador,
+#' @param tablero a matrix representing the state of the game board
+#' @param profundidad un entero que fija la profundidad del árbol de jugadas a analizar
+#' @param maximizandoIA Booleano. TRUE significa que se maximiza la puntuación de la IA
+#'   FALSE se minimiza la puntuación del jugador humano
+#' @param alpha parámetro de la poda alpha-beta. Por defecto -Inf
+#' @param beta parámetro de la poda alpha-beta. Por defecto +Inf
+
+#' @return returns a list with the following contents
+#' \itemize{
+#' \item{puntuacion}: puntuación optenida al evaluar la posición al realizar la 'jugada'
+#' \item{jugada}: jugada elegida por el algoritmo
+#' }
+#' @details
+#' \itemize{
+#' \item{If the game is over is TRUE}: result can be "WIN HUMAN", "WIN IA" or "DRAW’.
+#' \item{If the game is over is TRUE}: result is NA
+#' }
+#' @examples
+#' tablero <- crear_posicion_aleatoria(3)
+#' numNodos <- 0
+#' ts <- system.time(
+#'   minimax(tablero = tablero, profundidad = 7, maximizandoIA = TRUE)
+#' )
+#' numNodos
+#' numNodos /ts[3]
+
+minimax <- function(tablero, profundidad, maximizandoIA,
                                 alpha = -Inf, beta = Inf) {
 
   if (profundidad == 0 || juego_terminado(tablero)$finalizado) {
-    return(list(puntuacion = evaluar_posicion(tablero, 2) + evaluar_posicion(tablero, 1),
-                jugada = NA))
+    return(list(puntuacion = evaluar_posicion(tablero), jugada = NA))
   }
 
-  if (maximizando_jugador) {
+  if (maximizandoIA) {
     mejor_puntuacion <- -Inf
     mejor_jugada <- NA
 
     for (columna in jugadas_disponibles(tablero)) {
       nuevo_tablero <- realizar_jugada(tablero, columna, 2)
+      numNodos <<- numNodos + 1
 
-
-      puntuacion <- mejor_jugada_minmax(nuevo_tablero, profundidad - 1, FALSE,
-                                        alpha, beta)$puntuacion
+      puntuacion <- minimax(nuevo_tablero, profundidad - 1, FALSE, alpha, beta)$puntuacion
 
       if (puntuacion > mejor_puntuacion) {
         mejor_puntuacion <- puntuacion
@@ -32,10 +55,7 @@ mejor_jugada_minmax <- function(tablero, profundidad, maximizando_jugador,
       if (beta <= alpha) {
         break  # Poda alpha-beta
       }
-
-
     }
-
 
     return(list(puntuacion = mejor_puntuacion, jugada = mejor_jugada))
 
@@ -44,12 +64,11 @@ mejor_jugada_minmax <- function(tablero, profundidad, maximizando_jugador,
     mejor_puntuacion <- Inf
     mejor_jugada <- NA
 
-
     for (columna in jugadas_disponibles(tablero)) {
       nuevo_tablero <- realizar_jugada(tablero, columna, 1)
+      numNodos <<- numNodos + 1
 
-      puntuacion <- mejor_jugada_minmax(nuevo_tablero, profundidad - 1, TRUE,
-                                        alpha, beta)$puntuacion
+      puntuacion <- minimax(nuevo_tablero, profundidad - 1, TRUE, alpha, beta)$puntuacion
 
       if (puntuacion < mejor_puntuacion) {
         mejor_puntuacion <- puntuacion
