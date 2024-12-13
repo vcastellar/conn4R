9679eb33c24aa88148fe89cbf54864f0cd2c99d7
vcastellar <vic.sebastia@gmail.com>
2024-12-13 18:53:55 +0100

    definicion de clases y métodos

--- R/clases.R
+++ R/clases.R
@@ -1,40 +1,48 @@
 setClass("arbol",
-         slots = c(turno       = "logical",
+         slots = c(idNodo      = "integer",
                    jugada      = "integer",
+                   turno       = "logical",
                    profundidad = "integer",
-                   idNodo      = "integer"),
+                   puntuacion  = "numeric"),
          prototype = list(
-           turno       = c(),
+           idNodo      = c(),
            jugada      = c(),
+           turno       = c(),
            profundidad = c(),
-           idNodo      = c()
+           puntuacion  = c()
            )
          )
 
 setGeneric("actualizar", function(x, ...) standardGeneric("actualizar"))
 
 
-setClass("info", 
-         slots = c(numNodos = "integer",
-                   arbol = "arbol"
-                   ),
-         prototype = list(numNodos = 0L,
-                          arbol = new("arbol"))
-         )
-
-info <- new("info")
-str(info)
-
 # setMethod("inicializar", "arbol",
 #           function(.Object))
+
 setMethod("actualizar", "arbol", 
-          function(x = arbol, turno, jugada, profundidad) {
+          function(x = arbol, turno, jugada, profundidad, puntuacion) {
             x@turno       <- c(x@turno, turno)
             x@jugada      <- c(x@jugada, jugada)
             x@profundidad <- c(x@profundidad, profundidad)
             x@idNodo      <- c(x@idNodo, ifelse(is.null(x@idNodo[1]), 0L,  max(x@idNodo)) + 1L)
+            x@puntuacion  <- c(x@puntuacion, puntuacion)
             
             return(x)
             }
           )
+
+
+
+
+# # Definir el método específico para la clase arbol
+setGeneric("actUltNodo", function(obj, ...) standardGeneric("actUltNodo"))
+
+setMethod("actUltNodo", "arbol", function(obj, slotName, value) {
+  l <- length(slot(obj, slotName))
+  slot(obj, slotName)[l] <- value
+  return(obj)
+  })
+
+
+
 
