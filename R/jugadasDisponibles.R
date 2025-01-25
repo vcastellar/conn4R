5e276703fd29362759f8654ea8d7fc17c88a241f
vcastellar <vic.sebastia@gmail.com>
2025-01-25 11:15:47 +0100

    ordenar jugadas función de perdidas + ganancias

--- R/jugadasDisponibles.R
+++ R/jugadasDisponibles.R
@@ -4,11 +4,11 @@
 #'   jugadas existentes: columnas no completadas
 #' @param tablero matriz 6 x 7 que representa la situación del tablero de juego.
 #' @examples
-#' tablero <- crear_posicion_aleatoria(21)
-#' tablero <- readRDS("tableroPruebas")
+#' tablero <- crear_posicion_aleatoria(11)
+#' tablero <- readRDS("tableroPruebas.rds")
 #' visualizar_tablero(tablero)
 #' (jugadas_candidatas <- jugadas_disponibles(tablero))
-#' ordenar_jugadas(tablero, jugadas_candidatas)
+#' ordenar_jugadas(tablero, turno = 2)
 
 
 jugadas_disponibles <- function(tablero) {
@@ -20,18 +20,22 @@ jugadas_disponibles <- function(tablero) {
 
 # función ordenar jugadas de más prometedoras a menos
 ordenar_jugadas <- function(tablero, turno) {
-  puntuacion <- c()
   
   jugadas_candidatas <- jugadas_disponibles(tablero)
+  puntuacion <- rep(0, length(jugadas_candidatas))
   
-  for (i in jugadas_candidatas) {
-    tablero_aux <- realizar_jugada(tablero, i, turno)
-    puntuacion <- c(puntuacion, evaluar_posicion(tablero_aux))
+  # consecuencia de jugar columna i y de no jugar columna i
+  for (turno in c(1, 2)) {
+    for (i in 1:length(jugadas_candidatas)) {
+      columna <- jugadas_candidatas[i]
+      tablero_aux <- realizar_jugada(tablero, columna, turno)
+      puntuacion[i]  <- abs(puntuacion[i]) + abs(evaluar_posicion(tablero_aux))
+    }
   }
-  
+
   
   df <- data.frame(jugadas = jugadas_candidatas, puntuacion = puntuacion)
-  df <- df[order(df$puntuacion, decreasing = ifelse(turno == 2, TRUE, FALSE)), ]
+  df <- df[order(df$puntuacion, decreasing = TRUE), ]
   
   # poda de jugadas
   if (any(abs(df$puntuacion) > 10000)) {
