d3063e3fca44ae0e4f8f3b089eb98822432fb64a
vcastellar <vic.sebastia@gmail.com>
2025-02-24 20:08:29 +0100

    correcion linea ganadora

--- R/visualizarTablero.R
+++ R/visualizarTablero.R
@@ -36,7 +36,7 @@
           linea <- tablero[fila:(fila + 3), columna]
           puntuacion <- .evaluar_linea(linea, turno)
           
-          coordenadas <- data.frame(x = fila:(fila + 3), y = columna, ficha = 3)
+          coordenadas <- data.frame(x = 7 - (fila:(fila + 3)), y = columna, ficha = 3)
           
           if (puntuacion >= 10000) {
             stop()
@@ -50,7 +50,7 @@
           linea <- c(tablero[fila, columna], tablero[fila + 1, columna + 1],
                      tablero[fila + 2, columna + 2], tablero[fila + 3, columna + 3])
           
-          coordenadas <- data.frame(x = fila:(fila + 3), y = (columna:(columna + 3)), ficha = 3)
+          coordenadas <- data.frame(x = 7 - (fila:(fila + 3)), y = (columna:(columna + 3)), ficha = 3)
           
           puntuacion <- .evaluar_linea(linea, turno)
           if (puntuacion >= 10000) {
@@ -65,7 +65,7 @@
           linea <- c(tablero[fila, columna], tablero[fila + 1, columna - 1],
                      tablero[fila + 2, columna - 2], tablero[fila + 3, columna - 3])
           
-          coordenadas <- data.frame(x = fila:(fila + 3), y = (columna:(columna - 3)), ficha = 3)
+          coordenadas <- data.frame(x = 7 - (fila:(fila + 3)), y = (columna:(columna - 3)), ficha = 3)
           
           puntuacion <- .evaluar_linea(linea, turno)
           if (puntuacion >= 10000) {
@@ -122,7 +122,7 @@ visualizar_tablero <- function(tablero) {
   
   if (!is.null(linea)) {
     for (k in 1:4) {
-      r <- which(df_fichas$y == linea[k,]$x & df_fichas$x == linea[k, ]$y)
+      r <- which(df_fichas$x == linea[k,]$y & df_fichas$y == linea[k, ]$x)
       print(r)
       df_fichas$ficha[r] <- 3
       df_fichas$alpha[r] <- 1
