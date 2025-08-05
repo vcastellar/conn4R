9e725e0fe7a833772a00ff5d88c146da33b04a25
vcastellar <vic.sebastia@gmail.com>
2025-08-05 17:27:51 +0200

    cambios en el método de cálculo de la profundidad

--- R/profundidadAdaptativa.R
+++ R/profundidadAdaptativa.R
@@ -1,37 +1,51 @@
 #' profundidad adaptativa
 #' @examples
-#' profundidad <- 7
-#' numJugadas <- seq(7, 1, by = -1/7)
-#' profs <- .adaptativa(numJugadas, profundidad, alpha = 1.5)
-#' (numNodosAdap <- numJugadas^profs)
-#' (numNodosOrig <- numJugadas^profundidad)
-#' ylim = c(0, 1.1 * max(c(numNodosAdap, numNodosOrig)))
-#' plot(numJugadas, profs, type = "b")
-#' plot(numJugadas, numNodosAdap, type = 'b', pch = 19, ylim = ylim)
-#' lines(numJugadas, numNodosOrig, type = 'b', col = "red", pch = 19)
+#' tablero <- crear_posicion_aleatoria(10)
+#' .adaptativa(tablero)
 
 
+# .adaptativa <- function(numJugadas, profundidad, alpha = 1.5) {
+#   
+#   if (alpha < 1) {
+#     stop("parámetro alpha debe ser  a >= 1")
+#   }
+#   
+#   factor <- (1 / (numJugadas / 7))^(1/alpha)
+#   profs  <- round(profundidad * factor)
+#   prof   <- min(profs, round(6 * numJugadas))
+#   
+#   return(prof)
+# }
 
-.adaptativa <- function(numJugadas, profundidad, alpha = 1.5) {
-  
-  if (alpha < 1) {
-    stop("parámetro alpha debe ser  a >= 1")
-  }
-  
-  factor <- (1 / (numJugadas / 7))^(1/alpha)
-  profs  <- round(profundidad * factor)
-  prof   <- min(profs, round(6 * numJugadas))
-  
-  return(prof)
+# Cálculo de número de columnas disponibles (ramas posibles)
+.columnasValidas <- function(tablero) {
+  sum(apply(tablero, 2, function(x) {sum(x == 0) / 6}))
 }
 
 
-.adaptativa <- Vectorize(.adaptativa, vectorize.args = "numJugadas")
+.adaptativa <- function(tablero = tablero, profundidad_base = 5, profundidad_max = 15) {
+  b <- .columnasValidas(tablero)
+  if (b <= 1) return(profundidad_max)
+  
+  N <- 7 ^ profundidad_base
+  d_range <- 1:profundidad_max
+  nodos_estimados <- b^d_range
+  d_mejor <- d_range[which.min(abs(nodos_estimados - N))]
+  
+  return(d_mejor)
+}
 
 
+# .adaptativa <- Vectorize(.adaptativa, vectorize.args = "numJugadas")
+
+
+
+# .numJugadas <- function(tablero) {
+#   sum(apply(tablero, 2, function(x) {sum(x == 0) / 6}))
+# }
 
 .numJugadas <- function(tablero) {
-  sum(apply(tablero, 2, function(x) {sum(x == 0) / 6}))
+  sum(tablero != 0)
 }
 
 
