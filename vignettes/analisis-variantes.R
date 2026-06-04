c50f7082f6486af68344ab6ff57e2933ccbaca42
vcastellar <vic.sebastia@gmail.com>
2026-06-04 21:07:51 +0200

    viñeta

---
+++ vignettes/analisis-variantes.R
@@ -0,0 +1,12 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment  = "#>",
+  fig.width  = 6,
+  fig.height = 5
+)
+set.seed(123)
+
+## ----library------------------------------------------------------------------
+library(conn4R)
+
